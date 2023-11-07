b09129c7c09948af373505ab86097d66fbd9fd78
SAROSE <RAFATHSAROSEMRR@gmail.com>
2023-11-07 12:45:26 +0530

    launching

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".skills", {
+    strings: ["Software Engineer","Undergraduate","Full-Stake Developer","Tech Enthusiast"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop : true
+  });
