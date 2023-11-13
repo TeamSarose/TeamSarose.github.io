e596ca03313ba9cb1f9818db1f86cdb76ab50d1c
SAROSE <RAFATHSAROSEMRR@gmail.com>
2023-11-13 11:52:16 +0530

    Made Responsive

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-var typed = new Typed(".skills", {
+const menu = document.querySelector('.menu')      // Toggle Show Menu
+const menuList = document.querySelector('nav ul')
+
+menu.addEventListener('click',()=>{
+    menuList.classList.toggle('showMenu')
+})
+
+var typed = new Typed(".skills", {       // Type script for Skill Text
     strings: ["Software Engineer","Undergraduate","Full-Stake Developer","Tech Enthusiast"],
     typeSpeed: 100,
     backspeed: 100,
     backDelay: 1000,
     loop : true
-  });
+});
